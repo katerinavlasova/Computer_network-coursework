81c2d43f4624831fe195be37f179e77c2794a8d3
katerinavlasova <kate0112@mail.ru>
2020-12-27 21:47:07 +0300

    done..

--- ku.py
+++ ku.py
@@ -2,8 +2,10 @@
 
 import tkinter as tk
 from tkinter import messagebox  
+import datetime
 import time
 import sys
+
 try:
     import curses
 except ImportError:
@@ -12,6 +14,7 @@ import socket
 from socket import AF_INET, SOCK_STREAM, SOCK_DGRAM
 import psutil
 from psutil._common import bytes2human
+from psutil._compat import get_terminal_size
 from datetime import datetime
 import os
 import sys
@@ -239,6 +242,136 @@ def show_netinterface():
     inserter(text)
 
 
+def convert_bytes(n):
+    symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+    prefix = {}
+    for i, s in enumerate(symbols):
+        prefix[s] = 1 << (i + 1) * 10
+    for s in reversed(symbols):
+        if n >= prefix[s]:
+            value = float(n) / prefix[s]
+            return '%.1f%s' % (value, s)
+    return "%sB" % n
+
+def procsmem():
+    f = open('f.txt', 'w')
+    ad_pids = []
+    procs = []
+    for p in psutil.process_iter():
+        with p.oneshot():
+            try:
+                mem = p.memory_full_info()
+                info = p.as_dict(["cmdline", "username"])
+            except psutil.AccessDenied:
+                ad_pids.append(p.pid)
+            except psutil.NoSuchProcess:
+                pass
+            else:
+                p._uss = mem.uss
+                p._rss = mem.rss
+                if not p._uss:
+                    continue
+                p._pss = getattr(mem, "pss", "")
+                p._swap = getattr(mem, "swap", "")
+                p._info = info
+                procs.append(p)
+
+    procs.sort(key=lambda p: p._uss)
+    templ = "%-7s %-7s %7s %7s %7s %7s %7s"
+    print(templ % ("PID", "User", "USS", "PSS", "Swap", "RSS", "Cmdline"))
+    f.write(templ % ("PID", "User", "USS", "PSS", "Swap", "RSS", "Cmdline"))
+    print("=" * 78)
+    f.write('\n')
+    f.write("=" * 78)
+    for p in procs[:86]:
+        cmd = " ".join(p._info["cmdline"])[:50] if p._info["cmdline"] else ""
+        line = templ % (
+            p.pid,
+            p._info["username"][:7] if p._info["username"] else "",
+            convert_bytes(p._uss),
+            convert_bytes(p._pss) if p._pss != "" else "",
+            convert_bytes(p._swap) if p._swap != "" else "",
+            convert_bytes(p._rss),
+            cmd,
+        )
+        print(line)
+        f.write('\n')
+        f.write(line)
+    f.close()
+    text = open('f.txt', encoding='utf-8').readlines()
+    text = ''.join(text)
+    inserter(text)
+
+def ps():
+    f = open('f.txt', 'w')
+    today_day = datetime.today().date()
+    templ = "%-10s %5s %5s %7s %7s %5s %6s %6s %6s  %s"
+    attrs = ['pid', 'memory_percent', 'name', 'cmdline', 'cpu_times',
+             'create_time', 'memory_info', 'status', 'nice', 'username']
+    print(templ % ("USER", "PID", "%MEM", "VSZ", "RSS", "NICE",
+                   "STATUS", "START", "TIME", "CMDLINE"))
+    f.write(templ % ("USER", "PID", "%MEM", "VSZ", "RSS", "NICE",
+                   "STATUS", "START", "TIME", "CMDLINE"))
+    for p in psutil.process_iter(attrs, ad_value=None):
+        if p.info['create_time']:
+            ctime = datetime.fromtimestamp(p.info['create_time'])
+            if ctime.date() == today_day:
+                ctime = ctime.strftime("%H:%M")
+            else:
+                ctime = ctime.strftime("%b%d")
+        else:
+            ctime = ''
+        if p.info['cpu_times']:
+            cputime = time.strftime("%M:%S",
+                                    time.localtime(sum(p.info['cpu_times'])))
+        else:
+            cputime = ''
+
+        user = p.info['username']
+        if not user and psutil.POSIX:
+            try:
+                user = p.uids()[0]
+            except psutil.Error:
+                pass
+        if user and psutil.WINDOWS and '\\' in user:
+            user = user.split('\\')[1]
+        if not user:
+            user = ''
+        user = user[:9]
+        vms = bytes2human(p.info['memory_info'].vms) if \
+            p.info['memory_info'] is not None else ''
+        rss = bytes2human(p.info['memory_info'].rss) if \
+            p.info['memory_info'] is not None else ''
+        memp = round(p.info['memory_percent'], 1) if \
+            p.info['memory_percent'] is not None else ''
+        nice = int(p.info['nice']) if p.info['nice'] else ''
+        if p.info['cmdline']:
+            cmdline = ' '.join(p.info['cmdline'])
+        else:
+            cmdline = p.info['name']
+        status = p.info['status'][:5] if p.info['status'] else ''
+
+        line = templ % (
+            user,
+            p.info['pid'],
+            memp,
+            vms,
+            rss,
+            nice,
+            status,
+            ctime,
+            cputime,
+            cmdline)
+        print(line[:get_terminal_size()[0]])
+        f.write('\n')
+        f.write(line[:get_terminal_size()[0]])
+    f.close()
+    text = open('f.txt', encoding='utf-8').readlines()
+    text = ''.join(text)
+    inserter(text)
+
+def quit_program():
+	window.destroy()
 
 window = tk.Tk()
 window.geometry('800x600')
@@ -248,25 +381,48 @@ window.config(menu=mainmenu)
  
 domenu = tk.Menu(mainmenu, tearoff=0)
 domenu.add_command(label="Активные подключения", command = show_connections)
-domenu.add_command(label="Информация о памяти", command = info_memory)
-domenu.add_command(label="Сетев interface", command = show_netinterface)
-domenu.add_command(label="Монтированные диски", command = show_disks)
-domenu.add_command(label="Активные пользователи", command = show_users)
+#domenu.add_command(label="Информация о памяти", command = info_memory)
+domenu.add_command(label="Сетевые интерфейсы", command = show_netinterface)
+#domenu.add_command(label="Монтированные диски", command = show_disks)
+#domenu.add_command(label="Активные пользователи", command = show_users)
+#domenu.add_command(label="procsmem", command = procsmem)
+#domenu.add_command(label="ps", command = ps)
 domenu.add_separator()
-domenu.add_command(label="Выход")
+domenu.add_command(label="Выход", command = quit_program)
+
+
+memmenu = tk.Menu(mainmenu, tearoff=0)
+memmenu.add_command(label="Монтированные диски", command = show_disks)
+memmenu.add_command(label="Информация о памяти", command = info_memory)
+
+procmenu = tk.Menu(mainmenu, tearoff=0)
+procmenu.add_command(label="Память процессов", command = procsmem)
+procmenu.add_command(label="Состояние процессов", command = ps)
+
+usersmenu = tk.Menu(mainmenu, tearoff=0)
+usersmenu.add_command(label="Активные пользователи", command = show_users)
 
 infomenu = tk.Menu(mainmenu, tearoff=0)
 infomenu.add_command(label="О программе", command = get_info)
  
 
-mainmenu.add_cascade(label="Действия",
+mainmenu.add_cascade(label="Сеть",
                      menu=domenu)
+mainmenu.add_cascade(label="Процессы",
+                     menu=procmenu)
+mainmenu.add_cascade(label="Память",
+                     menu=memmenu)
+mainmenu.add_cascade(label="Пользователи",
+                     menu=usersmenu)
 mainmenu.add_cascade(label="Информация",
                      menu=infomenu)
 
 
 output = tk.Text(window, bg="white", padx = 4, width=900, height=900)
-output.grid(row=8, columnspan=99)
-
+#output.grid(row=8, columnspan=99)
+output.pack(side=tk.LEFT)
+scroll = tk.Scrollbar(command = output.yview)
+scroll.pack(side=tk.RIGHT, fill=tk.Y)
+output.config(yscrollcommand = scroll.set)
 window.mainloop()
 
